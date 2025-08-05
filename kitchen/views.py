1ab9c516ecc1e30556c2bc556c13cb7a7e914463
oleksa.shcherbakov <oleksa.shcherbakov@gmail.com>
2025-08-05 17:19:17 +0300

    made urls, views fir index, also templates (base, slidebar, index)

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Dish, Cook, DishType
+
+
+def index(request):
+    number_dishes = Dish.objects.count()
+    number_cooks = Cook.objects.count()
+    number_type_dishes = DishType.objects.count()
+
+    context = {
+        "number_dishes": number_dishes,
+        "number_cooks": number_cooks,
+        "number_type_dishes": number_type_dishes,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
