9a69c5b9d91bc6032637bbdb438c919253d6e603
oleksa.shcherbakov <oleksa.shcherbakov@gmail.com>
2025-08-15 11:18:52 +0300

    added pagination, some serialisers, added filtering to views and changed Models

--- API_kitchen/serializers.py
+++ API_kitchen/serializers.py
@@ -1,20 +1,46 @@
 from rest_framework import serializers
+from rest_framework.pagination import PageNumberPagination
 
 from kitchen.models import DishType, Dish, Cook
 
 
+class DishTypePagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 500
+
+
 class DishTypeSerializer(serializers.ModelSerializer):
+    pagination_class = DishTypePagination
 
     class Meta:
         model = DishType
         fields = ("id", "name", )
 
 
+class DishTypeSerializerWithoutId(serializers.ModelSerializer):
+
+    class Meta:
+        model = DishType
+        fields = ("name", )
+
+
 class DishSerializer(serializers.ModelSerializer):
+    dish_type = DishTypeSerializer()
 
     class Meta:
         model = Dish
         fields = ("id", "name", "description", "price", "is_expensive", "dish_type", "cooks", )
+
+    def create(self, validated_data):
+        dish_type_data = validated_data.pop("dish_type")
+        cooks_data = validated_data.pop("cooks", [])
+
+        dish_type_instance = DishType.objects.create(**dish_type_data)
+        dish = Dish.objects.create(dish_type=dish_type_instance, **validated_data)
+        dish.cooks.set(cooks_data)
+
+        return dish
 
 
 class CookSerializer(serializers.ModelSerializer):
@@ -23,3 +49,20 @@ class CookSerializer(serializers.ModelSerializer):
         model = Cook
         fields = ("id", "username", "email", "is_staff", "years_of_experience", )
         read_only_fields = ("is_staff", )
+
+
+    def validate(self, attrs):
+        if attrs["years_of_experience"] > 70:
+            raise serializers.ValidationError("You can't cook more than 70 years of experience")
+
+
+class CookSerializerForDishList(serializers.ModelSerializer):
+
+    class Meta:
+        model = Cook
+        fields = ("username", "years_of_experience", )
+
+
+class DishListSerializer(DishSerializer):
+    cooks = CookSerializerForDishList(many=True)
+    dish_type = DishTypeSerializerWithoutId()
--- API_kitchen/views.py
+++ API_kitchen/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
-from API_kitchen.serializers import DishTypeSerializer, DishSerializer, CookSerializer
+from API_kitchen.serializers import DishTypeSerializer, DishSerializer, CookSerializer, DishListSerializer
 from kitchen.models import DishType, Dish, Cook
 
 
@@ -9,12 +8,47 @@ class DishTypeViewSet(viewsets.ModelViewSet):
     queryset = DishType.objects.all()
     serializer_class = DishTypeSerializer
 
+    def get_queryset(self):
+        queryset = DishType.objects.all()
+        names = self.request.query_params.getlist("name")
+
+        if names:
+            queryset = queryset.filter(name__in=names)
+
+        return queryset.distinct()
+
 
 class DishViewSet(viewsets.ModelViewSet):
-    queryset = Dish.objects.all()
-    serializer_class = DishSerializer
+    queryset = Dish.objects.select_related().prefetch_related("cooks")
+
+    def get_queryset(self):
+        queryset = self.queryset
+        price = self.request.query_params.get("price")
+
+        if price:
+            queryset = self.queryset.filter(price__lt=price)
+            return queryset.distinct()
+
+        return queryset
+
+
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return DishListSerializer
+
+        return DishSerializer
 
 
 class CookViewSet(viewsets.ModelViewSet):
     queryset = Cook.objects.all()
     serializer_class = CookSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        years_of_experience = self.request.query_params.get("years_of_experience")
+
+        if years_of_experience:
+            return queryset.filter(years_of_experience__gte=years_of_experience)
+
+        return queryset
